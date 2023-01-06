46da61d65e69fa452453c04f3f1cc726572996ae
jmadrid221 <119139179+jmadrid221@users.noreply.github.com>
2023-01-06 10:18:45 +0100

    Update [[...slugs]].js

--- pages/posts/[[...slugs]].js
+++ pages/posts/[[...slugs]].js
@@ -47,7 +47,7 @@ export async function getStaticProps(Context) {
 
 
 
-    let data = await fetch('https://rupali.vercel.app/api/getMetadata', {
+    let data = await fetch('https://online72media.vercel.app/api/getMetadata', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
